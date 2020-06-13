19efe482e3127285a83973e4753ceaafa29660b1
PopckovS <popckovM5@yandex.com>
2020-06-14 01:43:11 +0300

    make it better

--- bot.py
+++ bot.py
@@ -190,7 +190,7 @@ def get_email(message):
     while not re.search(r'[\w.-]+@[\w.-]+\.?[\w]+?', email):
         result = re.search(r'[\w.-]+@[\w.-]+\.?[\w]+?', email)
         if result == None:
-            bot.send_message(message.from_user.id, 'Кажется, это неправильный email :( Попробуй еще раз!')
+            # bot.send_message(message.from_user.id, 'Кажется, это неправильный email :( Попробуй еще раз!')
 
     email = message.text
     bot.send_message(message.from_user.id, 'Ваш телефон ?')
@@ -210,7 +210,7 @@ def get_phone(message):
     while not re.search(r"\b\+?[7,8](\s*\d{3}\s*\d{3}\s*\d{2}\s*\d{2})\b", phone):
         result = re.search(r"\b\+?[7,8](\s*\d{3}\s*\d{3}\s*\d{2}\s*\d{2})\b", phone)
         if result == None:
-            bot.send_message(message.from_user.id, 'Кажется, это неправильный номер телефона :( Попробуй еще раз!')
+            # bot.send_message(message.from_user.id, 'Кажется, это неправильный номер телефона :( Попробуй еще раз!')
 
     phone = message.text
     bot.send_message(message.from_user.id, 'Расскажите о Вашем проекте')
