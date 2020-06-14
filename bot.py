9d8bfad7fee9ff0a9970dfeb83c81c84e88a2e66
PopckovS <popckovM5@yandex.com>
2020-06-15 02:41:56 +0300

    make it better

--- bot.py
+++ bot.py
@@ -7,9 +7,7 @@ import re # Импортирую модулья для работы с регу�
 import os
 from mitlabs import MitLabs # Импортирую класс с информацией о компании
 
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import smtplib
+
 
 
 # Создаем экземпляр класса для работы с библиотекой pyTelegramBotAPI, и передаем ему API токена.
@@ -62,33 +60,7 @@ def handle_docs_photo(message):
 
 @bot.message_handler(commands=['email'])
 def handle_email(message):
-    # create message object instance
-    msg = MIMEMultipart()
-
-    message = "Thank you"
-
-    # setup the parameters of the message
-    password = "IremOfPilars"
-    msg['From'] = "popckovM5@yandex.ru"
-    msg['To'] = "popckovM5@yandex.ru"
-    msg['Subject'] = "Subscription"
-
-    # add in the message body
-    msg.attach(MIMEText(message, 'plain'))
-
-    # create server
-    # server = smtplib.SMTP('smtp.yandex.ru: 587')
-    server = smtplib.SMTP('smtp.yandex.ru:465')
-
-    server.starttls()
-
-    # Login Credentials for sending the mail
-    server.login(msg['From'], password)
-
-    # send the message via the server.
-    server.sendmail(msg['From'], msg['To'], msg.as_string())
-
-    server.quit()
+    bot.send_message(message.chat.id, str(__name__), reply_markup=keyboard_inline)
 
 
 
